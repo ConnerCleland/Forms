1201effd8dcef9d36618b4128ab5bc7e8c00d10e
ConnerCleland <Chcleland2@gmail.com>
2023-12-01 15:02:28 -0600

    added

--- project/app/tests.py
+++ project/app/tests.py
@@ -14,19 +14,19 @@ class FrontTimesTestCase(TestCase):
 
         # Test case 1: Short input string
         front_times_response = self.client.post(
-            "/front_times/", {"input_string": "Chocolate", "n": 2}
+            "/warmup-2/font-times/", {"input_string": "Chocolate", "n": 2}
         )
         self.assertContains(front_times_response, "ChoCho")
 
         # Test case 2: Long input string
         front_times_response = self.client.post(
-            "/front_times/", {"input_string": "Chocolate", "n": 3}
+            "/warmup-2/font-times/", {"input_string": "Chocolate", "n": 3}
         )
         self.assertContains(front_times_response, "ChoChoCho")
 
         # Test case 3: Empty input string
         front_times_response = self.client.post(
-            "/front_times/", {"input_string": "Abc", "n": 3}
+            "/warmup-2/font-times/", {"input_string": "Abc", "n": 3}
         )
         self.assertContains(front_times_response, "AbcAbcAbc")
 
@@ -36,23 +36,27 @@ class XYZThereTestCase(TestCase):
         # Test the xyz_there_view with different inputs
 
         # Test case 1: xyz is present
-        xyz_there_response = self.client.post("/xyz_there/", {"input_string": "abcxyz"})
+        xyz_there_response = self.client.post(
+            "/string-2/xyz-there/", {"input_string": "abcxyz"}
+        )
         self.assertContains(xyz_there_response, "True")
 
         # Test case 2: xyz is not directly preceded by a period
         xyz_there_response = self.client.post(
-            "/xyz_there/", {"input_string": "abc.xyz"}
+            "/string-2/xyz-there/", {"input_string": "abc.xyz"}
         )
         self.assertContains(xyz_there_response, "False")
 
         # Test case 3: xyz is present and directly preceded by a period
         xyz_there_response = self.client.post(
-            "/xyz_there/", {"input_string": "xyz.abc"}
+            "/string-2/xyz-there/", {"input_string": "xyz.abc"}
         )
         self.assertContains(xyz_there_response, "True")
 
         # Test case 4: Short input string
-        xyz_there_response = self.client.post("/xyz_there/", {"input_string": "xyz"})
+        xyz_there_response = self.client.post(
+            "/string-2/xyz-there/", {"input_string": "xyz"}
+        )
         self.assertContains(xyz_there_response, "True")
 
 
@@ -62,19 +66,19 @@ class CenteredAverageTestCase(TestCase):
 
         # Test case 1: Regular input
         centered_average_response = self.client.post(
-            "/centered_average/", {"nums": "1, 2, 3, 4, 100"}
+            "/list-2/centered-average/", {"nums": "1, 2, 3, 4, 100"}
         )
         self.assertContains(centered_average_response, "3")
 
         # Test case 2: Multiple copies of smallest and largest values
         centered_average_response = self.client.post(
-            "/centered_average/", {"nums": "1, 1, 5, 5, 10, 8, 7"}
+            "/list-2/centered-average/", {"nums": "1, 1, 5, 5, 10, 8, 7"}
         )
         self.assertContains(centered_average_response, "5")
 
         # Test case 3: Negative values
         centered_average_response = self.client.post(
-            "/centered_average/", {"nums": "-10, -4, -2, -4, -2, 0"}
+            "/list-2/centered-average/", {"nums": "-10, -4, -2, -4, -2, 0"}
         )
         self.assertContains(centered_average_response, "-3")
 
@@ -85,25 +89,25 @@ class NoTeenSumViewTestCase(TestCase):
 
         # Test case 1: No teens present
         no_teen_sum_response = self.client.post(
-            "/no_teen_sum/", {"a": 1, "b": 2, "c": 3}
+            "/logic-2/no-teen-sum/", {"a": 1, "b": 2, "c": 3}
         )
         self.assertContains(no_teen_sum_response, "6")
 
         # Test case 2: Teen present, but not counting as 0
         no_teen_sum_response = self.client.post(
-            "/no_teen_sum/", {"a": 2, "b": 13, "c": 1}
+            "/logic-2/no-teen-sum/", {"a": 2, "b": 13, "c": 1}
         )
         self.assertContains(no_teen_sum_response, "3")
 
         # Test case 3: Teen present, counting as 0
         no_teen_sum_response = self.client.post(
-            "/no_teen_sum/", {"a": 2, "b": 1, "c": 14}
+            "/logic-2/no-teen-sum/", {"a": 2, "b": 1, "c": 14}
         )
         self.assertContains(no_teen_sum_response, "3")
 
         # Test case 4: All teens
         no_teen_sum_response = self.client.post(
-            "/no_teen_sum/", {"a": 14, "b": 15, "c": 19}
+            "/logic-2/no-teen-sum/", {"a": 14, "b": 15, "c": 19}
         )
         self.assertContains(no_teen_sum_response, "0")
 
--- project/project/urls.py
+++ project/project/urls.py
@@ -1,4 +1,3 @@
-# app/urls.py
 from django.urls import path
 from app.views import (
     front_times_view,
@@ -8,8 +7,8 @@ from app.views import (
 )
 
 urlpatterns = [
-    path("front_times/", front_times_view, name="front_times"),
-    path("no_teen_sum/", no_teen_sum_view, name="no_teen_sum"),
-    path("xyz_there/", xyz_there_view, name="xyz_there"),
-    path("centered_average/", centered_average_view, name="centered_average"),
+    path("warmup-2/font-times/", front_times_view, name="front_times"),
+    path("logic-2/no-teen-sum/", no_teen_sum_view, name="no_teen_sum"),
+    path("string-2/xyz-there/", xyz_there_view, name="xyz_there"),
+    path("list-2/centered-average/", centered_average_view, name="centered_average"),
 ]
